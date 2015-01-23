4c8e4f0b6d791d486e55b18652abef98087caaf7
Wooyoung Chung <yakle1218@gmail.com>
2015-01-23 15:59:00 -0800

    added validation on viewcounter

--- api/models/ViewCounter.js
+++ api/models/ViewCounter.js
@@ -12,5 +12,16 @@ module.exports = {
             model: 'Submission',
             required: true
         }
+    },
+
+    //prevent to create viewcounter where subid does not exist
+    beforeCreate: function(values, next){
+        Submission.findOne({id:values.submission_id}).exec(function(err, sub){
+            if(err)
+                return next(err);
+            if(!sub)
+                return next("submission id " + values.submission_id + " not exist");
+            next();
+        })
     }
 };
