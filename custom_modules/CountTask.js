c169935b68a9e18956a70e0f0cef28bbe399c50e
Wooyoung Chung <yakle1218@gmail.com>
2015-01-23 15:59:35 -0800

    implemented weekly run task

--- custom_modules/CountTask.js
+++ custom_modules/CountTask.js
@@ -5,11 +5,13 @@ module.exports = {
     runDailyTask: function() {
         console.log("setting up cronjob...");
         var job = new CronJob({
-            cronTime: '00 05 00 * * *', //run it everyday at 00:10:00 AM
+            cronTime: '*/10 * * * * *',
+            //cronTime: '00 15 00 * * *', //run it everyday at 04:00:00 AM
             onTick: function() {
+                 var q = 'SELECT distinct(submission_id), count(id) as ' + 
+                    'count from ViewCounter group by submission_id';
                  
-                 ViewCounter.query('SELECT distinct(submission_id), count(id) as count 
-                    from ViewCounter group by submission_id', function(err, views){
+                 ViewCounter.query(q, function(err, views){
         
                     if(err){
                         console.log('Error on access view counter');
@@ -27,7 +29,7 @@ module.exports = {
                     var success = true;
 
                     views.forEach(function(subs){ 
-                        Daily.create({date:Date(), count:subs.count, on:subs.submission_id})
+                        Daily.create({date:Date(), count:subs.count, subid:subs.submission_id})
                             .exec(function(err, daily){
                             if(err) {
                                 success = false;
@@ -35,8 +37,9 @@ module.exports = {
                                 return;
                             }
                         });
-                        sails.log(date + " -- " + subs.submission_id + " " + subs.count);
                     });
+
+                    //what about submission that doesn't have view today??? skip or insert with count 0? 
 
                     if(success){
                         ViewCounter.query('TRUNCATE TABLE ViewCounter', function(err, empty){
@@ -45,7 +48,7 @@ module.exports = {
                         });
                     }//what to do if theres error?? possibility?
                     
-                    console.log('Finished clearing job ' + views);
+                    console.log('Finished daily job');
                 });
                 
             },
@@ -59,11 +62,41 @@ module.exports = {
 
     runWeeklyTask: function() {
         var job = new CronJob({
-            cronTime: '00 00 00 * * 0',
+            cronTime: '*/10 * * * * *',
+            //cronTime: '00 15 01 * * 0',
             onTick: function() {
-                //get current date
-                //get last 7 days dailys indexes 
-                //count and insert new record in weekly 
+                var days = 7;
+                var today = new Date().toISOString().slice(0, 19).replace('T', ' ');
+                var date = new Date();
+                date.setTime(date.getTime() - (days * 24 * 60 * 60 * 1000));
+                date = date.toISOString().slice(0, 19).replace('T', ' ');
+                
+                var q = 'select distinct(subid), sum(count) from ' +
+                    '(select subid, count from daily where ' +
+                    'date between "' + date + '" and "' + today + '") as t group by subid';
+
+                console.log(q);
+
+                Daily.query(q, function(err, dailys){
+                    if(err){
+                        console.log('Database error while finding weekly data: ' + err);
+                        return;
+                    }
+
+                    if(!dailys || dailys.length == 0){
+                        console.log('Cannot find daily between ' + date + ' and ' + today);
+                        return;
+                    }
+
+                    //sum of count and id 
+                    Weekly.create({startDate:date, endDate:today, count: daily.count, subid:daily.subid})
+                        .exec(function(err, weekly){
+                        if(err)
+                            console.log('Cannot create weekly model');
+                    });
+            
+                });
+
             },
             start: false,
             timeZone: "America/Los_Angeles"
@@ -74,8 +107,9 @@ module.exports = {
 
     runMonthlyTask: function() {
         var job = new CronJob({
-            cronTime: '00 00 00 1 * *',
+            cronTime: '00 15 02 1 * *',
             onTick: function() {
+
                 //get current date
                 //get last 4 weekly indexes
                 //count and insert new record in monthly
