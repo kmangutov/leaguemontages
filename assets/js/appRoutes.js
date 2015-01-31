4727b7d5eea60af7828e0fb5823c4a2719da0d51
Wooyoung Chung <yakle1218@gmail.com>
2015-01-30 23:22:08 -0800

    set up appRoute temporary

---
+++ assets/js/appRoutes.js
@@ -0,0 +1,28 @@
+var route = angular.module('appRoutes', []);
+
+route.config(['$routeProvider', '$locationProvider',
+    function($routeProvider, $locationProvider){
+        $routeProvider.when('/', {
+            templateUrl: 'views/main.html'
+        })
+
+        .when('/admin', {
+            templateUrl: 'views/admin.html'
+        })
+
+        .when('/login', {
+            templateUrl: 'views/login.html'
+        })
+
+        .when('/logout', {
+            templateUrl: 'views/main.html'
+        })
+
+        //only care define route, otherwise 404
+        .otherwise({
+            templateUrl: 'views/404.html'
+        });
+
+
+    $locationProvider.html5Mode(true);
+  }]);
