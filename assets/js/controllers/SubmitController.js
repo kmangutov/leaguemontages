78bd9779bff92152143cf8b7e05a5ae661cf0cda
kmangutov <kmangutov@gmail.com>
2015-02-04 16:48:01 -0600

    submission works

--- assets/js/controllers/SubmitController.js
+++ assets/js/controllers/SubmitController.js
@@ -2,8 +2,8 @@
 var controllers = angular.module('appControllers', ['appServices']);
 
 controllers.controller("SubmitController", 
-  ['$scope', 'ChampionRoleService', 'ChampionService', 
-  function($scope, ChampionRoleService, ChampionService) {
+  ['$scope', 'ChampionRoleService', 'ChampionService', 'SubmissionService',
+  function($scope, ChampionRoleService, ChampionService, SubmissionService) {
 
     ChampionRoleService.query({}, function(championRoleService) {
       $scope.roles = championRoleService;
@@ -13,10 +13,16 @@ controllers.controller("SubmitController",
       $scope.champions = championService;
     });
 
-    $scope.postData = {};
+    $scope.postData = {
+      champ_type: 1,
+      champ_role: 1
+    };
 
     $scope.submit = function() {
       var stringData = JSON.stringify($scope.postData);
       console.log("PUT " + stringData); 
+
+      var submission = new SubmissionService($scope.postData);
+      submission.$save();
     }
 }]);
