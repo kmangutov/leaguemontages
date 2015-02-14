55c429da239d659cb0390e8750a700bd89f7f635
Wooyoung Chung <yakle1218@gmail.com>
2015-02-13 20:35:40 -0800

    defined badge/badgetype service
    counts method will return array of 2 objects that contains badge count
    info and mapping ids

--- assets/js/services/BadgeTypeService.js
+++ assets/js/services/BadgeTypeService.js
@@ -14,8 +14,8 @@ services.factory('BadgeTypeService',
 });
 
 services.factory('BadgeService',
-  function($resource) {
-    
+  function($resource, $http) {
+  /* 
   return $resource('/api/v1.0/Badge', {}, {
     qeury: {
       method: 'GET',
@@ -23,4 +23,46 @@ services.factory('BadgeService',
       isArray: true
     }
   });
+  */
+  var service = {};
+
+  service.get = $resource('/api/v1.0/Badge', {}, {
+      qeury: {
+        method: 'GET',
+        params: {},
+        isArray: true
+      }
+    }
+  );
+
+  //return promise and then portion will contains 
+  //count of each badges and map to badge_type id
+  service.counts = function(id){
+    var promise = 
+        $http.get('/api/v1.0/Badge?given_to='+id)
+             .then(function(res){
+                var badges = {};
+                var badgeTypeMap = {};
+                
+                angular.forEach(res.data, function(badge){
+                var key = badge.badge_type.name;
+
+                if(!(key in badges)){
+                    badges[key] = 1;
+                    badgeTypeMap[key] = badge.badge_type.id;
+                }
+                else
+                    badges[key] += 1;
+                });
+                console.log("service " + JSON.stringify(badges));
+
+                return {badges: badges, map:badgeTypeMap};
+              }, function(res){ //error
+                 return {status: res.status, data: res.data};
+              });
+
+    return promise;
+  };
+
+  return service;
 });
