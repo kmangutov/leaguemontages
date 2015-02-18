e928d76cf49c71a797da8f42c16bd84b4a79975f
Wooyoung Chung <yakle1218@gmail.com>
2015-02-17 23:01:55 -0800

    refactored getting badge counts

--- assets/js/services/BadgeTypeService.js
+++ assets/js/services/BadgeTypeService.js
@@ -15,19 +15,11 @@ services.factory('BadgeTypeService',
 
 services.factory('BadgeService',
   function($resource, $http) {
-  /* 
-  return $resource('/api/v1.0/Badge', {}, {
-    qeury: {
-      method: 'GET',
-      params: {},
-      isArray: true
-    }
-  });
-  */
+
   var service = {};
 
   service.get = $resource('/api/v1.0/Badge', {}, {
-      qeury: {
+      query: {
         method: 'GET',
         params: {},
         isArray: true
@@ -37,32 +29,57 @@ services.factory('BadgeService',
 
   //return promise and then portion will contains 
   //count of each badges and map to badge_type id
-  service.counts = function(id){
+  service.getBadgesBySubId = function(id){
     var promise = 
         $http.get('/api/v1.0/Badge?given_to='+id)
              .then(function(res){
                 var badges = {};
-                var badgeTypeMap = {};
+                //var badgeTypeMap = {};
                 
                 angular.forEach(res.data, function(badge){
                   var key = badge.badge_type.name;
 
                   if(!(key in badges)){
                     badges[key] = 1;
-                    badgeTypeMap[key] = badge.badge_type.id;
+                    //badgeTypeMap[key] = badge.badge_type.id;
                   }
                   else
                     badges[key] += 1;
                 });
                 console.log("service " + JSON.stringify(badges));
 
-                return {badges: badges, map:badgeTypeMap};
+                return badges;
               }, function(res){ //error
                 return {status: res.status, data: res.data};
               });
 
     return promise;
   };
+
+  service.getBadges = function(badges){
+    var retBadges = {};
+    var badgeTypeMap = {};
+
+    var promise = 
+      $http.get('/api/v1.0/BadgeType')
+           .then(function(badgetypes){
+              angular.forEach(badgetypes.data, function(badgetype){
+                badgeTypeMap[badgetype.id] = badgetype.name;
+                retBadges[badgetype.name] = {badgeCount:0, badgeId:badgetype.id};
+              });
+              console.log(JSON.stringify(retBadges));
+              angular.forEach(badges, function(badge){
+                var key = badge.badge_type;
+                if(badgeTypeMap[key] in retBadges)
+                  retBadges[badgeTypeMap[key]].badgeCount += 1;
+              }); 
+              return retBadges;
+           }, function(res){
+              return retBadges;
+           });
+
+    return promise;
+  }
 
   return service;
 });
