1047366f8deb5c3762b47a39b963a77d361831e6
Wooyoung Chung <yakle1218@gmail.com>
2015-03-04 19:26:34 +0900

    Revert b3640bb..4dc0f6e
    This rolls back to commit b3640bb3ad212597c6dbc527334dab66e6b1e677.

--- assets/js/controllers/FeedViewController.js
+++
@@ -1,9 +0,0 @@
-angular.module('appControllers').controller('FeedViewController', 
-    ['$scope','$window','$timeout', 'AuthService', 'UtilService',
-    function($scope, $window, $timeout, AuthService, UtilService){
-    
-      $scope.isLogged = AuthService.logState().isLogged;
-      console.log("feed view controller");
-      console.log($scope.isLogged);
-    }
-]);
